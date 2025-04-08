72e1c25ff6503a64146414b2d98bc69f1f58eb10
BrunaCLima <brunacardoso_lima@hotmail.com>
2025-04-08 20:23:48 +0100

    feat: adiciona rotas resource para o CRUD de contatos

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+Route::resource('contacts', ContactController::class);
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
+| Here is where you can register web routes for your application...
 */
 
 Route::get('/', function () {
