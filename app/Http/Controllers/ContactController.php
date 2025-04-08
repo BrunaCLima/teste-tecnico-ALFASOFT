00f24d95e10b069c205b451276b89140774af2f8
BrunaCLima <brunacardoso_lima@hotmail.com>
2025-04-08 19:54:37 +0100

    feat: implementa CRUD no ContactController

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        $contacts = Contact::all();
+        return response()->json($contacts);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $contact = Contact::create($request->all());
+        return response()->json($contact, 201);
+    }
+
+    public function show($id)
+    {
+        $contact = Contact::findOrFail($id);
+        return response()->json($contact);
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->update($request->all());
+        return response()->json($contact);
+    }
+
+    public function destroy($id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+        return response()->json(null, 204);
+    }
+}
+
